f9fbe5c64033aef53242bfb1fe0123276d1fc808
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-19 16:13:19 -0400

    6th

--- src/customers.js
+++ src/customers.js
@@ -1,4 +1,9 @@
-async (req, res) => {
+import pg from 'pg'
+import { creds } from '../creds.js'
+
+const { Pool } = pg
+
+export async function getAllCustomers (req, res) {
     const pool = new Pool(creds)
     const customers = await pool.query('SELECT * FROM customers')
         .catch(err => res.status(500).send(er))
