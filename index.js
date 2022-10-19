a0d88a94dcca5c2198f4144e2ac191280f0da022
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-19 14:06:19 -0400

    3rd

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import pg from 'pg'
+import {creds} from './creds.js'
+
+const {Pool} = pg
 
 const app = express()
 
-
+    app.get('/customers', async (req, res) => {
+    const pool = new Pool(creds)
+    const customers = await pool.query('SELECT * FROM customers')
+    .catch(err => res.status(500).send(er))
+    res.send(customers.rows)
+    pool.end()
+})
 
 
 app.listen(3030, () => console.log('Listening on http://localhost:3030...'))
